425ad3f2b8a8f2953da528b57fc5de2dbc30dcd7
Czacker <1209892890@qq.com>
2018-08-09 17:22:09 +0800

    reques index data

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,84 +4,45 @@ Page({
    * 页面的初始数据
    */
   data: {
-    newstip:[ { name: '国内'}, {name: '国际'}, {name: '财经'}, {name: '娱乐'}, {name: '军事'}, {name: '体育'}, {name:'其他'}],
-    hotnews: { title: 'title', source: 'source', time: 'time', firstImage:'http://img1.gtimg.com/news/pics/hv1/38/85/1076/69988613.jpg'},
-    newdata: [{
-      "id": 1519631218506,
-      "title": "外媒称香港回归15年打破“经济将死”预言",
-      "date": "2012-07-01",
-      "source": "中国新闻网",
-      "firstImage": "http://img1.gtimg.com/news/pics/hv1/38/85/1076/69988613.jpg"
-    },
-      {
-        "id": 1519631218591,
-        "title": "实德指媒体窃公司机密 已向某媒体递律师函",
-        "date": "2012-04-21",
-        "source": "腾讯财经",
-        "firstImage": "http://img1.gtimg.com/finance/pics/hv1/33/207/1023/66573393.jpg"
-      },
-      {
-        "id": 1519631218595,
-        "title": "公务员医疗费用成迷 学者呼吁管理应公开透明",
-        "date": "2012-02-26",
-        "source": "财新网",
-        "firstImage": "http://img1.gtimg.com/finance/pics/hv1/241/102/983/63945826.jpg"
-      }]
+    newstip: [{ name: '国内', id: 'gn' }, { name: '国际', id: 'gj' }, { name: '财经', id: 'cj' }, { name: '娱乐', id: 'yl' }, { name: '军事', id: 'js' }, { name: '体育', id: 'ty' }, { name: '其他', id: 'other'}],
+    hotnews: [],
+    newdata: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.getNews()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
+  onPullDownRefresh() {
+    this.getNews(() => {
+      wx.stopPullDownRefresh()
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
+  getNews(callback) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data: {
+        type: 'gn'
+      },
+      success: res => {
+        let result = res.data.result
+        this.setNews(result)
+      },
+      complete: () => {
+        callback && callback()
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
+  setNews(result) {
+    console.log(result)
+    result.map( u => { u.date = u.date.split('T')[0] })
+    let hotnewdata = result[0];
+    let newsdata = result.shift();
+    this.setData({
+      hotnews: hotnewdata,
+      newdata: result
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  }
 })
