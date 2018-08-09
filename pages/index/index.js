f067e25a437c71264301c8d716d0a569df7ea2cd
Czacker <1209892890@qq.com>
2018-08-09 13:17:40 +0800

    finish index css

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,29 @@ Page({
    * 页面的初始数据
    */
   data: {
-    newstip:[ { name: '国内'}, {name: '国际'}, {name: '财经'}, {name: '娱乐'}, {name: '军事'}, {name: '体育'}, {name:'其他'}]
+    newstip:[ { name: '国内'}, {name: '国际'}, {name: '财经'}, {name: '娱乐'}, {name: '军事'}, {name: '体育'}, {name:'其他'}],
+    hotnews: { title: 'title', source: 'source', time: 'time', firstImage:'http://img1.gtimg.com/news/pics/hv1/38/85/1076/69988613.jpg'},
+    newdata: [{
+      "id": 1519631218506,
+      "title": "外媒称香港回归15年打破“经济将死”预言",
+      "date": "2012-07-01",
+      "source": "中国新闻网",
+      "firstImage": "http://img1.gtimg.com/news/pics/hv1/38/85/1076/69988613.jpg"
+    },
+      {
+        "id": 1519631218591,
+        "title": "实德指媒体窃公司机密 已向某媒体递律师函",
+        "date": "2012-04-21",
+        "source": "腾讯财经",
+        "firstImage": "http://img1.gtimg.com/finance/pics/hv1/33/207/1023/66573393.jpg"
+      },
+      {
+        "id": 1519631218595,
+        "title": "公务员医疗费用成迷 学者呼吁管理应公开透明",
+        "date": "2012-02-26",
+        "source": "财新网",
+        "firstImage": "http://img1.gtimg.com/finance/pics/hv1/241/102/983/63945826.jpg"
+      }]
   },
 
   /**
